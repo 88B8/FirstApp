25a73f3030c3f54d270b7ca76b28604a5a80f601
88B8 <egoboosted@proton.me>
2026-01-17 13:26:59 +0300

    added image, button, text and throwing dice function

--- app/src/main/java/firstapp/kamenskiy/MainActivity.kt
+++ app/src/main/java/firstapp/kamenskiy/MainActivity.kt
@@ -1,10 +1,13 @@
 package firstapp.kamenskiy
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener { view -> changeText() }
+    }
+
+    fun changeText() {
+        val resultTextView: TextView = findViewById(R.id.mainText)
+        resultTextView.text = throwDice().toString()
+    }
+
+    fun throwDice() : Int {
+        return Random.nextInt(1, 7)
     }
 }
